bfbc322e3746035533464e0c9fcdd7825a08b696
Artem <artctvcode@gmail.com>
2023-03-12 20:55:32 +0500

    update tests code for using unittest

--- task1/src/tests/model_testing.py
+++ task1/src/tests/model_testing.py
@@ -5,29 +5,40 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 from tqdm import tqdm
+import unittest
 
-BUDGET = 10
 
+class TestModel(unittest.TestCase):
 
-def check_budget(source_path, attack_patch, quantile_path):
+    SOURCE_PATH: str = "data/external/transactions.csv"
+    ATTACK_PATH: str = "data/processed/non_naive_submission.csv"
+    QUANTILE_PATH: str = "model/quantiles.json"
 
-    with open(quantile_path, "rb") as f:
-        quantiles = json.load(f)
+    BUDGET: int = 10
 
-    df_source = pd.read_csv(
-        source_path,
-        parse_dates=["transaction_dttm"],
-        dtype={"user_id": int, "mcc_code": int, "currency_rk": int, "transaction_amt": float},
-    ).sort_values(by=["user_id", "transaction_dttm"])
+    def setUp(self) -> None:
+        with open(self.QUANTILE_PATH, "rb") as f:
+            self.quantiles = json.load(f)
 
-    df_attack = pd.read_csv(
-        attack_patch,
-        parse_dates=["transaction_dttm"],
-        dtype={"user_id": int, "mcc_code": int, "currency_rk": int, "transaction_amt": float},
-    ).sort_values(by=["user_id", "transaction_dttm"])
+        self.df_source = pd.read_csv(
+            self.SOURCE_PATH,
+            parse_dates=["transaction_dttm"],
+            dtype={"user_id": int, "mcc_code": int, "currency_rk": int, "transaction_amt": float},
+        ).sort_values(by=["user_id", "transaction_dttm"])
 
-    assert df_source.shape == df_attack.shape
+        self.df_attack = pd.read_csv(
+            self.ATTACK_PATH,
+            parse_dates=["transaction_dttm"],
+            dtype={"user_id": int, "mcc_code": int, "currency_rk": int, "transaction_amt": float},
+        ).sort_values(by=["user_id", "transaction_dttm"])
 
+        if self.df_source.shape != self.df_attack.shape:
+            self.fail("Some error message")
+
+    def tearDown(self) -> None:
+        pass
+
+    @staticmethod
     def is_different_records(a, b):
         return not all(
             [
@@ -39,47 +50,32 @@ def check_budget(source_path, attack_patch, quantile_path):
             ]
         )
 
-    diff_count = defaultdict(int)
+    def test_correct_budget(self) -> None:
+        diff_count = defaultdict(int)
+        print("Start testing")
+        for a, b in tqdm(
+            zip(self.df_source.itertuples(), self.df_attack.itertuples()),
+            total=self.df_source.shape[0]
+        ):
+            if self.is_different_records(a, b):
+                diff_count[a.user_id] += 1
+                self.assertGreaterEqual(self.BUDGET, diff_count[a.user_id], msg="budget over")
 
-    for a, b in tqdm(zip(df_source.itertuples(), df_attack.itertuples()), total=df_source.shape[0]):
-        if is_different_records(a, b):
-            diff_count[a.user_id] += 1
-            if diff_count[a.user_id] > BUDGET:
-                print('budget over')
-                return False
+                self.assertEqual(
+                    np.sign(a.transaction_amt), np.sign(b.transaction_amt),
+                    msg="sign changed"
+                )
 
-            if np.sign(a.transaction_amt) != np.sign(b.transaction_amt):
-                print('sign change')
-                return False
+                if a.transaction_amt < 0:
+                    ruler = self.quantiles["negative"]
+                else:
+                    ruler = self.quantiles["positive"]
 
-            if a.transaction_amt < 0:
-                ruler = quantiles["negative"]
-            else:
-                ruler = quantiles["positive"]
-            #key_a = str(a.mcc_code)
-            key_b = str(b.mcc_code)
-            #upper_bound_a = ruler["max"][key_a]
-            #lower_bound_a = ruler["min"][key_a]
-            upper_bound_b = ruler["max"][key_b]
-            lower_bound_b = ruler["min"][key_b]
-            if any(
-                [
-                    #upper_bound_a < a.transaction_amt,
-                    upper_bound_b < b.transaction_amt,
-                    #lower_bound_a > a.transaction_amt,
-                    lower_bound_b > b.transaction_amt,
-                ]
-            ):
-                print('amt exceeded')
-                return False
-    return True
+                key_b = str(b.mcc_code)
+                upper_bound_b = ruler["max"][key_b]
+                lower_bound_b = ruler["min"][key_b]
 
-
-def main():
-    source_path = "data/external/transactions.csv"
-    attack_patch = "data/processed/non_naive_submission.csv"
-    quantile_path = "model/quantiles.json"
-    print(check_budget(source_path, attack_patch, quantile_path))
-
-if __name__ == "__main__":
-    main()
+                self.assertFalse(
+                    any([upper_bound_b < b.transaction_amt, lower_bound_b > b.transaction_amt]),
+                    msg="amt exceeded"
+                )
