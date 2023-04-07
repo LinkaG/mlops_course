dd0c95b67e5977d8abd2b2d0a2fdb10cb53b12e9
Artem 'artctv <artctvcode@gmail.com>
2023-04-07 21:37:32 +0500

    some fixes and optimizations

--- lab3/app.py
+++ lab3/app.py
@@ -3,34 +3,33 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-# Загружаем обученную модель
+
 app = FastAPI()
+
+# Загружаем обученную модель
 model = joblib.load("./model.joblib")
+
 
 class IrisSpecies(BaseModel):
     """
-    класс для валидации входных данных.
+    Схема входных данных
     """
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
 
-@app.post('/predict') # описываем постзапрос
+
+@app.post('/predict')  # описываем постзапрос
 def predict(iris: IrisSpecies):
     """
-    на вход принимаем фичи и делаем предсказания.
+    Эндпоинт принимает фичи и делает предсказания
     """
-    features = [[
-        iris.sepal_length,
-        iris.sepal_width,
-        iris.petal_length,
-        iris.petal_width
-    ]]
+    features = [list(iris.dict().values())]
     prediction = model.predict(features).tolist()[0]
     return {"prediction": prediction}
 
 
 if __name__=='__main__':
     # запускаем сервер
-    uvicorn.run(app, host='127.0.0.1', port = 80)
+    uvicorn.run(app, host='127.0.0.1', port=5000)
